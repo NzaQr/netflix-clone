d6389fcb22a1d63e11b55e767164c46a19a044c5
Nazareno Quiroga <naz.quiroga@gmail.com>
2021-01-18 14:42:22 -0300

    jumbotron styled

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -6,6 +6,7 @@ import {
   Title,
   SubTitle,
   Image,
+  Pane,
 } from "./styles/jumbotron";
 
 export default function Jumbotron({
@@ -30,6 +31,10 @@ Jumbotron.Title = function JumbotronTitle({ children, ...restProps }) {
 
 Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
   return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }) {
+  return <Pane {...restProps}>{children}</Pane>;
 };
 
 Jumbotron.Image = function JumbotronImage({ ...restProps }) {
