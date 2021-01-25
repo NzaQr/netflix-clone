b53e85da0e4bb2ba97c910fe0aff1408f7bbb277
Nazareno Quiroga <naz.quiroga@gmail.com>
2021-01-25 18:59:28 -0300

    firebase auth setup

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { FirebaseContext } from "./context/firebase";
 const config = {
   apiKey: "AIzaSyB_m4rteqKkdMfB4L-B_bl0HAC3i-cBVHA",
   authDomain: "neflix-clone-594c2.firebaseapp.com",
+  databaseURL: "https://neflix-clone-594c2-default-rtdb.firebaseio.com",
   projectId: "neflix-clone-594c2",
   storageBucket: "neflix-clone-594c2.appspot.com",
   messagingSenderId: "790598797620",
