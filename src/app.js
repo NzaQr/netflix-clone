6cb9f10dd29c95edd334d65746270d19a2463268
Nazareno Quiroga <naz.quiroga@gmail.com>
2021-01-25 14:54:08 -0300

    sign up page

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import { Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
-import { Home, Signin } from "./pages";
+import { Home, Signin, Signup } from "./pages";
 export function App() {
   return (
     <Router>
@@ -11,7 +11,7 @@ export function App() {
           <Signin />
         </Route>
         <Route path={ROUTES.SIGN_UP}>
-          <p>Sign up page</p>
+          <Signup />
         </Route>
         <Route path={ROUTES.BROWSE}>
           <p>Browse page</p>
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
-import { Form } from "../components/";
 import { HeaderContainer } from "../containers/header";
+import { Form } from "../components/";
 import { FooterContainer } from "../containers/footer";
+import * as ROUTES from "../constants/routes";
 
-export default function Signin() {
-  const [error, setError] = useState("");
+export default function Signup() {
+  const [firstName, setFirstName] = useState("");
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const isInvalid = password === "" || emailAddress === "";
-  const handleSignin = (event) => {
+
+  const handleSignup = (event) => {
     event.preventDefault();
   };
 
@@ -17,9 +20,14 @@ export default function Signin() {
     <>
       <HeaderContainer>
         <Form>
-          <Form.Title>Sign In</Form.Title>
+          <Form.Title>Sign Up</Form.Title>
           {error && <Form.Error>{error}</Form.Error>}
-          <Form.Base onSubmit={handleSignin} method="POST">
+          <Form.Base onSubmit={handleSignup} method="POST">
+            <Form.Input
+              placeholder="First name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
             <Form.Input
               placeholder="Email address"
               value={emailAddress}
@@ -33,11 +41,11 @@ export default function Signin() {
               onChange={({ target }) => setPassword(target.value)}
             />
             <Form.Submit disabled={isInvalid} type="submit">
-              Sign In
+              Sign Up
             </Form.Submit>
 
             <Form.Text>
-              New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+              Have an account? <Form.Link to="/signin">Sign in now.</Form.Link>
             </Form.Text>
             <Form.TextSmall>
               This page is protected by Google reCAPTCHA.
